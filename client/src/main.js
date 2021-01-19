fb9f17ba9636380fab5756bbe6dad278402c938a
lihx <ilihuaxi@gmail.com>
2021-01-19 21:07:00 +0800

    build: WDS

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,14 @@ import './lib/jq-extend';
 import { debounce } from './lib/utils';
 import './js/index';
 
+// if (module.hot) {
+//   module.hot.accept('./main.js', function () {
+//     console.log('Accepting the updated printMe module!');
+//   });
+//   // 关闭指定子模块的HMR
+//   // module.hot.decline('./extra.js')
+// }
+
 const oInput = $('#key-ipt');
 const oForecastList = $('#forecast');
 
