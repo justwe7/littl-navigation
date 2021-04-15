048e74a05c9062cdc377c3a20c0e18983351d8a6
justwe7 <ilihuaxi@gmail.com>
2021-04-15 23:12:08 +0800

    feat: 短链页面

--- client/src/js/modules/slink.js
+++ client/src/js/modules/slink.js
@@ -1,1 +1,39 @@
-console.log(3333);
+import './slink.scss';
+
+$('.shortlink_hook').on('click', function (e) {
+  e.preventDefault();
+  let errFlag = false;
+  const formData = $('#form').serializeArray().reduce((result, target) => {
+    if (target.value === '' || target.value === undefined) {
+      errFlag = true;
+    }
+    result[target.name] = target.value;
+    return result;
+  }, {});
+  if (errFlag) {
+    $('#errorModalTips').html('请输入要生成的短链的域名');
+    $('#errorModal').modal('show');
+    return;
+  }
+  const $this = $(this);
+  $this.attr('disabled', true).html(`
+  <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                  Loading...
+  `);
+  const params = {
+    url: formData.protocol + formData.url
+  };
+  // console.log(formData, params);
+  $.ajax({
+    url: 'https://t.littl.cn/api/gen',
+    // url: 'http://localhost:7778/api/gen',
+    type: 'post',
+    data: params,
+    success: function (response) {
+      $this.attr('disabled', false).html('生成');
+      $('#gModalTips').html(response.data);
+      $('#gModal').modal('show');
+      // console.log(response);
+    }
+  });
+});
--- client/src/main.js
+++ client/src/main.js
@@ -3,20 +3,6 @@ import './lib/jq-extend';
 import './js/index';
 import './js/search';
 
-$('.shortlink_hook').on('click', function (e) {
-  e.preventDefault();
-  $.ajax({
-    url: 'https://t.littl.cn/api/gen',
-    // url: 'http://localhost:7778/api/gen',
-    type: 'post',
-    data: { url: $('#url').val().trim() },
-    success: function (response) {
-      alert(JSON.stringify(response));
-      console.log(response);
-    }
-  });
-});
-
 // if (module.hot) {
 //   module.hot.accept('./main.js', function () {
 //     console.log('Accepting the updated printMe module!');
